e1dc52421d71f74afc2a9e4957c9bac1627eb960
sravella <sravella@osius.com>
2025-01-06 11:07:11 +0530

    First commit

---
+++ src/main/java/com/fs/ecom/ecom_webapp/EcomWebappApplication.java
@@ -0,0 +1,13 @@
+package com.fs.ecom.ecom_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fs/ecom/ecom_webapp/EcomWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fs.ecom.ecom_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
