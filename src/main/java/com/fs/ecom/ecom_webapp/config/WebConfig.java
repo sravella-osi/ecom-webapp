838f0949981b2cf155bee0db609b83c9085c1de6
sravella <sravella@osius.com>
2025-01-10 16:44:30 +0530

    bean added to webconfi

--- src/main/java/com/fs/ecom/ecom_webapp/config/WebConfig.java
+++ src/main/java/com/fs/ecom/ecom_webapp/config/WebConfig.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override public void addCorsMappings(CorsRegistry registry) {
